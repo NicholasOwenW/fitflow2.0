e9d758a7dbdf851f049e56058c7b27cba6bad8ef
NicholasOwenW <nicholas.owen@student.umn.ac.id>
2023-12-13 14:58:34 +0700

    Committing
    Commit1st

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "fitflow2.0"
+include(":app")
+ 
