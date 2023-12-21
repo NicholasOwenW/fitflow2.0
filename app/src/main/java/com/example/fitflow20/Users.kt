66fb4dac78612b02f9f0ab6b058da2d1200dbcbb
NicholasOwenW <nicholas.owen@student.umn.ac.id>
2023-12-21 11:22:29 +0700

    Squashed commit of the following:
    commit fcf83656970344b09e1f63c20313e052cee593a7
    Author: lietristan <115028927+lietristan@users.noreply.github.com>
    Date:   Thu Dec 21 07:14:22 2023 +0700
    
        7 13 12 21 struktur data baru, tapi workoutdays ga muncul di firebase
    
    commit c3b9381e3b2ed7885768deb0c9aee70bb8715423
    Author: lietristan <115028927+lietristan@users.noreply.github.com>
    Date:   Thu Dec 21 05:10:00 2023 +0700
    
        jam 5 pagi 12 21 23 schedmenu sama scheduler 80%
    
        scheduler menu = menu hari
        scheduler = list workout yang tampil abis pencet hari
    
    commit 1b973b4ffc30426094e707ab489285a820be5ffd
    Author: lietristan <115028927+lietristan@users.noreply.github.com>
    Date:   Wed Dec 20 22:02:52 2023 +0700
    
        api harusnya done, tapi error di adapter

--- app/src/main/java/com/example/fitflow20/Users.kt
+++ app/src/main/java/com/example/fitflow20/Users.kt
@@ -1,11 +1,12 @@
 package com.example.fitflow20
 data class Users(
-    val id: String,
-    val userNama: String? = null,
-    val email: String?,
-    val phoneNumber: String,
-    val gender : String
-) {
-    constructor():this("","","","","")
+    val uid: String="",
+    val userNama: String = "",
+    val email: String = "",
+    val phoneNumber: String = "",
+    val workoutDay: Map<String, WorkoutDay> = emptyMap()
+)
 
-}
+data class WorkoutDay(
+    val listOfWorkouts: List<String> = emptyList()
+)
